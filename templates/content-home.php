ddf7de764f372a003203436efc6bf42bf9565661
Colin Waddell <mrcolin@gmail.com>
2013-09-24 20:57:19 +0100

    added homepage structure

---
+++ templates/content-home.php
@@ -0,0 +1,56 @@
+
+
+
+<section id="home-blurb">
+  <?php while (have_posts()) : the_post(); ?>
+    <div class="row">
+
+      <div class="col-xs-8">
+        <?php the_content(); ?>
+      </div>  
+
+    </div>
+  <?php endwhile; ?>
+</section> <!-- home-blurb -->
+
+
+
+
+<section id="link-bar">
+
+</section> <!-- link bar -->
+
+
+
+
+
+
+
+<section id="websites">
+
+</section> <!-- websites -->
+
+
+
+
+
+
+
+<section id="projects">
+
+</section> <!-- projects -->
+
+
+
+
+
+
+
+
+
+<section id="contact">
+
+</section> <!-- contact -->
+
+
+
