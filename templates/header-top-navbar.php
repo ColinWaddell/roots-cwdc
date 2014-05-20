88cfcd6279c3c6bcd24490c917efd857786205a8
Colin Waddell <mrcolin@gmail.com>
2014-05-20 22:54:26 +0100

    sorted icon links

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -32,7 +32,7 @@
         <div class="row">
 
           <div class="col-xs-4">
-            <a class="link-icon" href="" data-toggle="tooltip" title="facebook">
+            <a class="link-icon" href="https://www.facebook.com/colindwaddell" data-toggle="tooltip" title="facebook">
               <img src="<?php echo site_url('/assets/img/facebook.svg'); ?>" alt="">
             </a>
           </div>
@@ -42,7 +42,7 @@
             </a>
           </div>
           <div class="col-xs-4">
-            <a class="link-icon" href="" data-toggle="tooltip" title="twitter">
+            <a class="link-icon" href="https://twitter.com/ColinWaddellCom" data-toggle="tooltip" title="twitter">
               <img src="<?php echo site_url('/assets/img/twitter.svg'); ?>" alt="">
               </a>
           </div>
@@ -53,17 +53,17 @@
         <div class="row">
 
           <div class="col-xs-4">
-            <a class="link-icon" href="" data-toggle="tooltip" title="google+">
+            <a class="link-icon" href="https://plus.google.com/115058664570473226586" data-toggle="tooltip" title="google+">
               <img src="<?php echo site_url('/assets/img/google-plus.svg'); ?>" alt="">
             </a>
           </div>
           <div class="col-xs-4">
-            <a class="link-icon" href="" data-toggle="tooltip" title="github">
+            <a class="link-icon" href="https://github.com/ColinWaddell/" data-toggle="tooltip" title="github">
               <img src="<?php echo site_url('/assets/img/github.svg'); ?>" alt="">
             </a>
           </div>
           <div class="col-xs-4">
-            <a class="link-icon" href="" data-toggle="tooltip" title="instagram">
+            <a class="link-icon" href="http://instagram.com/mrwarmtoast" data-toggle="tooltip" title="instagram">
               <img src="<?php echo site_url('/assets/img/instagram.svg'); ?>" alt="">
             </a>
           </div>
