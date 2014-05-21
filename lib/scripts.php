13a71245120b096c7173b55948c23ca0828f161f
Colin Waddell <mrcolin@gmail.com>
2014-05-21 23:35:05 +0100

    fixed up mobile

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f7b18b21deb6c0c11aa345cba0e547a4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3c1a2f6531b481b4b638d32f1ca1ef06');
   wp_enqueue_style('nv_d3_css', get_template_directory_uri() . '/assets/js/plugins/github-calendar/css/nv.d3.css');
   wp_enqueue_style('gitgraph_css', get_template_directory_uri() . '/assets/js/plugins/github-calendar/css/github-calendar.css');
 
