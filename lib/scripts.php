b74654f6a506fd398e6eace8997b1d6b665cd0fe
Colin Waddell <mrcolin@gmail.com>
2014-05-22 00:13:52 +0100

    removed unecessary js and css

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,8 +12,6 @@
  */
 function roots_scripts() {
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a027a73a1ef4b9b48da848a8d72aa37b');
-  wp_enqueue_style('nv_d3_css', get_template_directory_uri() . '/assets/js/plugins/github-calendar/css/nv.d3.css');
-  wp_enqueue_style('gitgraph_css', get_template_directory_uri() . '/assets/js/plugins/github-calendar/css/github-calendar.css');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
