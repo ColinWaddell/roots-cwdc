dfbe4021b7b1373edcd7249aabc976c9fa31aad4
Colin Waddell <mrcolin@gmail.com>
2014-05-26 19:35:29 +0100

    made 404 link orange

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <h3>I recently moved everything on my site, sorry</h3>
 
 <p>
-  <a href="<?php echo site_url();?>" class="btn btn-primary btn-lg active" role="button">Head to my Homepage, everything's there now &rarr;</a>
+  <a href="<?php echo site_url();?>" class="btn btn-warning btn-lg active" role="button">Head to my Homepage, everything's there now &rarr;</a>
 </p>
 
 
