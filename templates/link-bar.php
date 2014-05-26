f7a369ed68a97a98cbb53d3a2f03d2415bc2cc3b
Colin Waddell <mrcolin@gmail.com>
2014-05-26 19:05:19 +0100

    link bar in sperate sheet

---
+++ templates/link-bar.php
@@ -0,0 +1,42 @@
+<div class="link-bar">
+  <div class="row">
+
+    <div class="col-xs-4">
+      <a class="link-icon" href="https://www.facebook.com/colindwaddell" data-toggle="tooltip" title="facebook">
+        <img src="<?php echo site_url('/assets/img/facebook.svg'); ?>" alt="">
+      </a>
+    </div>
+    <div class="col-xs-4">
+      <a class="link-icon" href="https://www.linkedin.com/profile/view?id=207368864" data-toggle="tooltip" title="linked in">
+        <img src="<?php echo site_url('/assets/img/linkedin.svg'); ?>" alt="">
+      </a>
+    </div>
+    <div class="col-xs-4">
+      <a class="link-icon" href="https://twitter.com/ColinWaddellCom" data-toggle="tooltip" title="twitter">
+        <img src="<?php echo site_url('/assets/img/twitter.svg'); ?>" alt="">
+        </a>
+    </div>
+
+  </div> <!-- row -->
+
+
+  <div class="row">
+
+    <div class="col-xs-4">
+      <a class="link-icon" href="https://plus.google.com/115058664570473226586" data-toggle="tooltip" title="google+">
+        <img src="<?php echo site_url('/assets/img/google-plus.svg'); ?>" alt="">
+      </a>
+    </div>
+    <div class="col-xs-4">
+      <a class="link-icon" href="https://github.com/ColinWaddell/" data-toggle="tooltip" title="github">
+        <img src="<?php echo site_url('/assets/img/github.svg'); ?>" alt="">
+      </a>
+    </div>
+    <div class="col-xs-4">
+      <a class="link-icon" href="http://instagram.com/mrwarmtoast" data-toggle="tooltip" title="instagram">
+        <img src="<?php echo site_url('/assets/img/instagram.svg'); ?>" alt="">
+      </a>
+    </div>
+
+ </div> <!-- row -->
+</div>
