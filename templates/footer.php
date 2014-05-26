6b86cbb35c2b30af847ce58bc14b9908a3586ab8
Colin Waddell <mrcolin@gmail.com>
2014-05-26 18:54:59 +0100

    link bar looks much nicer on mobile

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,8 @@
 <footer class="content-info container" role="contentinfo">
+  <div class="visible-xs">
+    <?php get_template_part('templates/link-bar');?>
+  </div>
+
   <div class="row">
     <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -6,52 +6,9 @@
         <h1><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></h1>
         <h2><a href="<?php echo site_url(); ?>"><?php bloginfo('description'); ?></a></h2>
       </div>
-      <div class="col-sm-4 pull-right" id="link-bar">
-        <div class="row">
-
-          <div class="col-xs-4">
-            <a class="link-icon" href="https://www.facebook.com/colindwaddell" data-toggle="tooltip" title="facebook">
-              <img src="<?php echo site_url('/assets/img/facebook.svg'); ?>" alt="">
-            </a>
-          </div>
-          <div class="col-xs-4">
-            <a class="link-icon" href="https://www.linkedin.com/profile/view?id=207368864" data-toggle="tooltip" title="linked in">
-              <img src="<?php echo site_url('/assets/img/linkedin.svg'); ?>" alt="">
-            </a>
-          </div>
-          <div class="col-xs-4">
-            <a class="link-icon" href="https://twitter.com/ColinWaddellCom" data-toggle="tooltip" title="twitter">
-              <img src="<?php echo site_url('/assets/img/twitter.svg'); ?>" alt="">
-              </a>
-          </div>
-
-        </div> <!-- row -->
-
-
-        <div class="row">
-
-          <div class="col-xs-4">
-            <a class="link-icon" href="https://plus.google.com/115058664570473226586" data-toggle="tooltip" title="google+">
-              <img src="<?php echo site_url('/assets/img/google-plus.svg'); ?>" alt="">
-            </a>
-          </div>
-          <div class="col-xs-4">
-            <a class="link-icon" href="https://github.com/ColinWaddell/" data-toggle="tooltip" title="github">
-              <img src="<?php echo site_url('/assets/img/github.svg'); ?>" alt="">
-            </a>
-          </div>
-          <div class="col-xs-4">
-            <a class="link-icon" href="http://instagram.com/mrwarmtoast" data-toggle="tooltip" title="instagram">
-              <img src="<?php echo site_url('/assets/img/instagram.svg'); ?>" alt="">
-            </a>
-          </div>
-
-       </div> <!-- row -->
-
-     </div>
+      <div class="col-sm-4 pull-right hidden-xs">
+        <?php get_template_part('templates/link-bar');?>
+      </div>
     </div>
-
-
-
   </div>
 </div>
